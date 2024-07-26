c79c48014d9750a426d45ad0374dc13dea0c1b55
ragapriya <ragapriya@DESKTOP-SOAHGTG>
2024-07-26 15:56:08 +0530

    This is my first java project

---
+++ Javabasic1/src/Day1/AdditionalDemo.java
@@ -0,0 +1,18 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class AdditionalDemo {
+
+	public static void main(String[] args) {
+		int a,b,c;
+	    Scanner s = new Scanner(System.in);
+		String t = s.nextLine();
+		a=s.nextInt();
+		b=s.nextInt();
+		c=a+b;
+		System.out.println("Additional:"+c);
+        System.out.println("name:"+t);
+	}
+
+}
